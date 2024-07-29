a560a09a50cc915f583c17498696c87758755794
Tai874vn <135409751+Tai874vn@users.noreply.github.com>
2024-07-29 23:21:59 +0700

    quick save

--- components/Saideptrieu.vue
+++ components/Saideptrieu.vue
@@ -1,84 +1,131 @@
 <template> 
-
-      <div class="super-box-1 d-flex justify-space-between" >
-          <v-sheet class="left d-flex flex-column" width="40%">
-
-          <p class="features">
+  <div class="super-duper-box">
+      <div class="super-box-1 d-flex flex-row">
+        <div class="left d-flex flex-column" width="40%">
+          <p class="pa1">
             FEATURES
           </p>
-          <p class="solve">
-            Solve Your Staffing Challenges
+          <p class="pa2">
+            Solve Your Staffing
           </p>
-          <p class="our">
+          <p class="pa3">
+            Challenges
+          </p>
+          <p class="pa4">
             Our plateform provides tailored features that effortlessly address your staffing needs, ensuring the right talent is always ready for you.
           </p>
-          <v-sheet class="but">
-            <p class="get">
+          <v-sheet class="but d-flex" justify="row">
+            <p>
               Get Started
             </p>
-            <p class="try">
+            <p>
               Try JobInn Today !
             </p>
           </v-sheet>
-        </v-sheet>
-
-          <v-sheet class="right" width="40%">
-
-        <div class="all-4">
-          <v-container class="d-flex">
-            <v-card class="c1" outlined rounded>
-              <div>
-                <img src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/66307ea94b1a01a34c45b64b_public.svg">
-              </div>
-              <v-card-title>
-                Automated Smart Matching
-              </v-card-title>
-              <v-card-text>
-                Instantly connect with the perfect candidates for your needs.
-              </v-card-text>
-            </v-card>
-            <v-card class="c2" outlined rounded>
-              <div>
-                <img src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/66307ea94b1a01a34c45b631_offline_bolt.svg">
-              </div>
-              <v-card-title>
-                Training and Badge Certification
-              </v-card-title>
-              <v-card-text>
-                Equip your workforce with specific skills, verified through certification.
-              </v-card-text>
-            </v-card>
-          </v-container>
-          </div>
-          <div class="big-card-2 d-flex ">
-            <v-card class="c3" outlined rounded>
-              <div>
-                <img src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/66307eaa4b1a01a34c45b653_password.svg">
-              </div>
-              <v-card-title>
-                Compliance and Risk Management
-              </v-card-title>
-              <v-card-text>
-                Stay compliant with all employment laws and regulations.
-              </v-card-text>
-            </v-card>
-            <v-card class="c4" outlined rounded>
-              <div>
-                <img src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/66307ea94b1a01a34c45b622_timelapse.svg">
-              </div>
-              <v-card-title>
-                Flexible Workforce Access
-              </v-card-title>
-              <v-card-text>
-                Access skilled on-demand workers to match your dynamic staffing needs.
-              </v-card-text>
-            </v-card>
-          </div>
-        </v-sheet>
-
         </div>
+        <div class="right" width="40%" >
+            <v-container class="big-c1 d-flex">
+             <v-card class="c1 ma-md-4 mr-lg-16" outlined shaped elevation="3" width="40%">
+                <div>
+                  <img src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/66307ea94b1a01a34c45b64b_public.svg" class="ma-md-4 ma-lg-4 mb-lg-0">
+                </div>
+                <v-card-title class="title1">
+                 Automated Smart Matching
+                </v-card-title>
+                <v-card-text>
+                  Instantly connect with the perfect candidates for your needs.
+                </v-card-text>
+              </v-card>
+              <v-card class="c2 ma-md-4" outlined shaped elevation="3" width="40%">
+                <div>
+                  <img src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/66307ea94b1a01a34c45b631_offline_bolt.svg" class="ma-md-4 ma-lg-4 mb-lg-0">
+                </div>
+                <v-card-title class="title2">
+                 Training and Badge Certification
+                </v-card-title>
+                <v-card-text>
+                 Equip your workforce with specific skills, verified through certification.
+                </v-card-text>
+              </v-card>
+            </v-container>
+            <v-container class="big-c2 d-flex">
+              <v-card class="c3 ma-md-4 mr-lg-16" outlined shaped elevation="3" width="40%">
+                <div>
+                  <img src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/66307eaa4b1a01a34c45b653_password.svg" class="ma-md-4 ma-lg-4 mb-lg-0">
+                </div>
+                <v-card-title class="title3">
+                  Compliance and Risk Management
+                </v-card-title>
+                <v-card-text>
+                  Stay compliant with all employment laws and regulations.
+                </v-card-text>
+              </v-card>
+              <v-card class="c4 ma-md-4" outlined shaped elevation="3" width="40%">
+                <div>
+                  <img src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/66307ea94b1a01a34c45b622_timelapse.svg" class="ma-md-4 ma-lg-4 mb-lg-0">
+                </div>
+                <v-card-title class="title4">
+                 Flexible Workforce Access
+                </v-card-title>
+                <v-card-text>
+                  Access skilled on-demand workers to match your dynamic staffing needs.
+                </v-card-text>
+              </v-card>
+            </v-container>
+        </div>
+        </div>
+      <div class="super box 2">
       </div>
-
+  </div>
 </template>
 <style> 
+.title1 {
+  font-size:22px;
+}
+.title2 {
+  font-size:22px;
+}
+.title4 {
+  font-size:22px;
+}
+.pa1{
+   margin-top: 95px;
+   letter-spacing: .08em;
+    text-transform: uppercase;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 1.8;
+    font-family: Claritycity, sans-serif;
+    color: #5e6672;
+}
+.pa2{
+  font-size: 48px;
+    font-style: normal;
+    line-height: 1.2;
+    font-family: Claritycity, sans-serif;
+    font-weight: 700;
+}
+.pa3{
+  font-size: 48px;
+    font-style: normal;
+    line-height: 1.2;
+    font-family: Claritycity, sans-serif;
+    font-weight: 700;
+    padding-bottom: 0px;
+}
+.pa4{
+  color: var(--neutral--800);
+    letter-spacing: -.02em;
+    margin-bottom: 0;
+    font-size: 16px;
+    line-height: 1.8;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    unicode-bidi: isolate;
+    font-family: Claritycity, sans-serif;
+    font-weight: 500;
+}
 </style>
